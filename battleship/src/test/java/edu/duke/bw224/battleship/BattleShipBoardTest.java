107d2fa593ee64a5ca42aec0cef697cc316315de
boyi <bw224@duke.edu>
2023-01-24 12:16:09 -0500

    create BattleShipBoard

---
+++ battleship/src/test/java/edu/duke/bw224/battleship/BattleShipBoardTest.java
@@ -0,0 +1,25 @@
+package edu.duke.bw224.battleship;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class BattleShipBoardTest {
+
+    @Test
+    void test_width_and_height() {
+        Board b1 = new BattleShipBoard(10, 20);
+        assertEquals(10, b1.getWidth());
+        assertEquals(20, b1.getHeight());
+    }
+
+    @Test
+    public void test_invalid_dimensions() {
+        assertThrows(IllegalArgumentException.class, () -> new BattleShipBoard(10, 0));
+        assertThrows(IllegalArgumentException.class, () -> new BattleShipBoard(0, 20));
+        assertThrows(IllegalArgumentException.class, () -> new BattleShipBoard(10, -5));
+        assertThrows(IllegalArgumentException.class, () -> new BattleShipBoard(-8, 20));
+    }
+
+
+}
